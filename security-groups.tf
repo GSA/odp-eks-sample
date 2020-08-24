07260c8a7c2dd719acae7229218b512e8b6b9f7c
RobertJLupinek <9854828608@GSA.GOV>
2020-08-24 17:22:05 -0400

    Correcting tfsec findings for security groups.

--- security-groups.tf
+++ security-groups.tf
@@ -2,6 +2,7 @@
 resource "aws_security_group" "worker_group_mgmt_one" {
   name_prefix = "${var.project}-worker_group_mgmt_one"
   vpc_id      = module.vpc.vpc_id
+  description = "Management for EKS worker group one"
 
   ingress {
     from_port = 22
@@ -17,6 +18,7 @@ resource "aws_security_group" "worker_group_mgmt_one" {
 resource "aws_security_group" "worker_group_mgmt_two" {
   name_prefix = "${var.project}-worker_group_mgmt_two"
   vpc_id      = module.vpc.vpc_id
+  "Management for EKS worker group two"
 
   ingress {
     from_port = 22
@@ -32,6 +34,7 @@ resource "aws_security_group" "worker_group_mgmt_two" {
 resource "aws_security_group" "all_worker_mgmt" {
   name_prefix = "${var.project}-all_worker_management"
   vpc_id      = module.vpc.vpc_id
+  "Management for EKS worker nodes"
 
   ingress {
     from_port = 22
