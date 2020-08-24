a1b803c6ea278de66e416a0c1639887191695f5a
RobertJLupinek <9854828608@GSA.GOV>
2020-08-24 16:43:20 -0400

    First commit.

---
+++ variables.tf
@@ -0,0 +1,12 @@
+variable "project" {
+    default = "odp-eks"
+    type = string
+    description = "Variable used for naming prefix and tagging."
+}
+
+
+variable "region" {
+    default = "us-east-1"
+    type = string
+    description = "Variable used for defining region."
+}
