48dd0d8302f1654009c3c9012e65a28689ac9d58
RobertJLupinek <9854828608@GSA.GOV>
2020-08-25 10:17:54 -0400

    Configuring the s3 backend.

--- main.tf
+++ main.tf
@@ -1,3 +1,19 @@
+variable "region" {
+  default     = ${var.region}
+  description = "AWS region"
+}
+
 provider "aws" {
-  region  = "us-east-1"
+  version = ">= 2.28.1"
+  region  = ${var.region}
+}
+
+
+terraform {
+  backend "s3" {
+    #These configuration points are set in CI as they cannot be interpolated.
+    #bucket = "mybucket"
+    #key    = "path/to/my/key"
+    #region = "us-east-1"
+  } 
 }
