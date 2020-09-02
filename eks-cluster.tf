25982276e54b3c26fa18cae4aedfd56fc02abf4b
RobertJLupinek <9854828608@GSA.GOV>
2020-09-02 15:13:19 -0400

    Stripping out second cluster.

--- eks-cluster.tf
+++ eks-cluster.tf
@@ -19,14 +19,7 @@ module "eks" {
       additional_userdata           = "echo foo bar"
       asg_desired_capacity          = 2
       additional_security_group_ids = [aws_security_group.worker_group_mgmt_one.id]
-    },
-    {
-      name                          = "${var.project}-worker-group-2"
-      instance_type                 = "t2.medium"
-      additional_userdata           = "echo foo bar"
-      additional_security_group_ids = [aws_security_group.worker_group_mgmt_two.id]
-      asg_desired_capacity          = 1
-    },
+    }
   ]
 }
 
--- security-groups.tf
+++ security-groups.tf
@@ -15,21 +15,7 @@ resource "aws_security_group" "worker_group_mgmt_one" {
   }
 }
 
-resource "aws_security_group" "worker_group_mgmt_two" {
-  name_prefix = "${var.project}-worker_group_mgmt_two"
-  vpc_id      = module.vpc.vpc_id
-  description = "Management for EKS worker group two"
 
-  ingress {
-    from_port = 22
-    to_port   = 22
-    protocol  = "tcp"
-
-    cidr_blocks = [
-      "192.168.0.0/16",
-    ]
-  }
-}
 
 resource "aws_security_group" "all_worker_mgmt" {
   name_prefix = "${var.project}-all_worker_management"
@@ -43,8 +29,7 @@ resource "aws_security_group" "all_worker_mgmt" {
 
     cidr_blocks = [
       "10.0.0.0/8",
-      "172.16.0.0/12",
-      "192.168.0.0/16",
+      "172.16.0.0/12"
     ]
   }
 }
