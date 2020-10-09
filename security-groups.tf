7d46542e9918922fb52537558e85d1d7246a985e
RobertJLupinek <9854828608@GSA.GOV>
2020-10-09 14:48:11 -0400

    Adding security groups for Pods and NameSpaces

--- security-groups.tf
+++ security-groups.tf
@@ -1,4 +1,3 @@
-
 resource "aws_security_group" "worker_group_mgmt_one" {
   name_prefix = "${var.project}-worker_group_mgmt_one"
   vpc_id      = module.vpc.vpc_id
@@ -14,8 +13,6 @@ resource "aws_security_group" "worker_group_mgmt_one" {
     ]
   }
 }
-
-
 
 resource "aws_security_group" "all_worker_mgmt" {
   name_prefix = "${var.project}-all_worker_management"
@@ -33,3 +30,58 @@ resource "aws_security_group" "all_worker_mgmt" {
     ]
   }
 }
+
+resource "aws_security_group" "eks_ns_default" {
+  name_prefix = "${var.project}-eks_ns_default"
+  vpc_id      = module.vpc.vpc_id
+  description = "Security Group for default namespace"
+
+}
+
+resource "aws_security_group" "eks_ns_app1" {
+  name_prefix = "${var.project}-eks_ns_app1"
+  vpc_id      = module.vpc.vpc_id
+  description = "Security Group for app1 namespace"
+
+  ingress {
+    from_port = 80
+    to_port   = 80
+    protocol  = "tcp"
+
+    security_groups = [
+        aws_security_group.eks_ns_app2.id
+    ]
+  }
+}
+
+resource "aws_security_group" "eks_ns_app2" {
+  name_prefix = "${var.project}-eks_ns_app2"
+  vpc_id      = module.vpc.vpc_id
+  description = "Security Group for app2 namespace"
+
+  ingress {
+    from_port = 80
+    to_port   = 80
+    protocol  = "tcp"
+
+    security_groups = [
+      aws_security_group.eks_ns_default.id
+    ]
+  }
+}
+
+resource "aws_security_group" "eks_pod_app2" {
+  name_prefix = "${var.project}-eks_pod_app2"
+  vpc_id      = module.vpc.vpc_id
+  description = "Security Group for app2 pod"
+  ingress {
+    from_port = 80
+    to_port   = 80
+    protocol  = "tcp"
+
+    security_groups = [
+        aws_security_group.eks_ns_app2.id
+    ]
+  }
+}
+
